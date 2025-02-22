29b3d477d7d78a7fd68d27519f843c88df73b12b
owenstack <jenukohaefobi@gmail.com>
2025-02-22 15:52:15 +0100

    Refactor Telegram client initialization and add login function

--- actions/telegram.ts
+++ actions/telegram.ts
@@ -4,14 +4,21 @@ import { env } from "@/lib/env";
 import { TelegramClient } from "telegram";
 import { StoreSession } from "telegram/sessions";
 
+const session = new StoreSession("sessions");
+const client = new TelegramClient(
+    session,
+    env.TELEGRAM_APP_ID,
+    env.TELEGRAM_APP_HASH,
+    { connectionRetries: 5 },
+);
+
 export async function authorize() {
-	const session = new StoreSession("sessions");
-	const client = new TelegramClient(
-		session,
-		env.TELEGRAM_APP_ID,
-		env.TELEGRAM_APP_HASH,
-		{ connectionRetries: 5 },
-	);
 	await client.connect();
 	return await client.checkAuthorization();
+}
+
+export async function login({phone, code, password}: {phone: string, code: string, password?: string}) {
+    if (!authorize()) {
+        
+    }
 }
